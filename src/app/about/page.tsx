59bef1b36ae29dead9adafe96a555b304f72f085
hiimintt <nlee0075@student.monash.edu>
2026-02-03 10:09:42 +0700

    [Edit] Change folder name from about to about us

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import AboutUsSection from "@/components/ui/about-us-section"
-
-export default function AboutPage() {
-  return (
-    <div className="min-h-screen">
-      <AboutUsSection />
-    </div>
-  );
-}
